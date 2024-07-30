7b194876f6a7f4b1af35ba45997186b333638a6d
devjmv <javier161890@gmail.com>
2024-07-30 21:06:18 +0200

    add navBar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 </script>
 
 <template>
-  <header>
+  <NavBar/>
+  <header class="mt-5">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
---
+++ src/components/NavBar.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="container mt-2 fixed-top">
+        <nav class="navbar navbar-expand-lg bg-dark rounded" data-bs-theme="dark" aria-label="Twelfth navbar">
+            <div class="container-fluid">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarsExample10" aria-controls="navbarsExample10" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-md-center" id="navbarsExample10">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Centered nav only</a>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/" class="nav-link">Home</RouterLink>
+                        </li>
+                        <li class="nav-item">
+                            <RouterLink to="/about" class="nav-link">About</RouterLink>
+                        </li>
+                        <li class="nav-item dropdown invisible">
+                            <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown"
+                                aria-expanded="false">Dropdown</a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<style scoped></style>
